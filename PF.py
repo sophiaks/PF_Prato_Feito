6725c9a354812d0610b2e77f6069e4c9cd2ce421
Sophia <sophia.shigueoka@gmail.com>
2019-05-07 16:10:48 -0300

    Update PF.py

--- PF.py
+++ PF.py
@@ -1,3 +1,7 @@
 #PROJETO FINAL
-
+import pygame
 #PRIMEIRA PARTE: BACKGROUND
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        player_img = pygame.image.load(path.join(img_dir, "")).convert()
