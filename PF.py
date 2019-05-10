36c70ca0799d05878a0c229aac014fae6eb7b231
Sophia <sophia.shigueoka@gmail.com>
2019-05-10 13:59:41 -0300

    Update PF.py

--- PF.py
+++ PF.py
@@ -1,8 +1,8 @@
 import pygame
 from os import path
 img_dir = path.join(path.dirname(__file__), 'img')
-WIDTH = 480  # Largura da tela
-HEIGHT = 600  # Altura da tela
+WIDTH = 1000  # Largura da tela
+HEIGHT = 1000  # Altura da tela
 FPS = 60  # Frames por segundo
 # Define algumas variáveis com as cores básicas
 WHITE = (255, 255, 255)
@@ -17,18 +17,19 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         player_img = pygame.image.load(
-            path.join(img_dir, "esteira.png")).convert()
+            path.join(img_dir, "esteirapixel.png")).convert()
         self.image = player_img
         self.image = pygame.transform.scale(player_img, (50, 38))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
-
+        all_sprites = pygame.sprite.Group()
         screen.fill(BLACK)
         screen.blit(background, background_rect)
         all_sprites.draw(screen)
-        pygame.displayflip()
+        pygame.display.flip()
+
 
 
 # Inicialização do Pygame.
@@ -46,7 +47,6 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 # Comando para evitar travamentos.
 try:
-
     # Loop principal.
     running = True
     while running:
