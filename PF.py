c6abd959e508560812b253db031f694eb7b77bb2
Sophia <sophia.shigueoka@gmail.com>
2019-05-14 16:36:26 -0300

    Update PF.py

--- PF.py
+++ PF.py
@@ -1,17 +1,10 @@
 import pygame
-from os import path
-img_dir = path.join(path.dirname(__file__), 'img')
-WIDTH = 1000  # Largura da tela
-HEIGHT = 600  # Altura da tela
-FPS = 60  # Frames por segundo
-# Define algumas variáveis com as cores básicas
 import sys
-
+from os import path
 from pygame.locals import *
 
 # Inicializa o jogo
 pygame.init()
-
 # FPS e o Clock
 FPS = 120
 fpsClock = pygame.time.Clock()
@@ -23,24 +16,20 @@ screen = pygame.display.set_mode((1000, 750), 0, 32)
 pygame.display.set_caption('Burrito Animado')
 
 burrito = pygame.transform.scale(
-    (pygame.image.load('tortilla.png')), (100, 100))
+    (pygame.image.load('tortilla.png')), (200, 200))
 
 bx = -150
 by = 400
 
-
 # cores
 BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
 RED = (255,   0,   0)
 GREEN = (0, 255,   0)
 BLUE = (0,   0, 255)
 OLIVE = (128, 128, 0)
+
+# Desenhando o retângulo de ingredientes
 
 # Classe de ingredientes?
 
@@ -51,11 +40,6 @@ class Ingredientes(pygame.sprite.Sprite):
 
     def __init__(self, color, x, y):
         pygame.sprite.Sprite.__init__(self)
-        player_img = pygame.image.load(
-            path.join(img_dir, "burrito.png")).convert()
-        self.image = player_img
-        self.image = pygame.transform.scale(player_img, (50, 38))
-        self.image.set_colorkey(BLACK)
         self.x = y
         self.y = x
         self.color = color
@@ -63,49 +47,11 @@ class Ingredientes(pygame.sprite.Sprite):
             (Ingredientes.larg_ing, Ingredientes.alt_ing))
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        all_sprites = pygame.sprite.Group()
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
-        pygame.display.flip()
         self.rect.x = self.x
         self.rect.y = self.y
 
 
-# Inicialização do Pygame.
-pygame.init()
-pygame.mixer.init()
-# Tamanho da tela.
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# Nome do jogo
-pygame.display.set_caption("FamFam")
-# Carrega o fundo do jogo
-player = Player()
 all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
-# Comando para evitar travamentos.
-try:
-    # LOOP PRINCIPAL
-    running = True
-    while running:
-
-        # Processa os eventos (mouse, teclado, botão, etc).
-        for event in pygame.event.get():
-            all_sprites.update()
-
-            # Mecanismo de click
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #    mx, my == pygame.mouse.get_pos()
-            #    if mx <  and mx > and my > and my < :
-
-            # A cada loop, redesenha o fundo e os sprites
-    screen.fill(BLACK)
-    screen.blit(background, background_rect)
-    # Depois de desenhar tudo, inverte o display.
-    pygame.display.flip()
-finally:
-    pygame.quit()
 ing1 = Ingredientes(RED, 100, 150)
 ing2 = Ingredientes(BLUE, 100, 450)
 ing3 = Ingredientes(GREEN, 100, 750)
@@ -117,7 +63,7 @@ screen.fill(WHITE)
 # Loop principal
 
 while True:
-    bx += 3
+    bx += 1
     screen.blit(burrito, (bx, by))
 
     for event in pygame.event.get():
