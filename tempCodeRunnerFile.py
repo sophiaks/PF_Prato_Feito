accd8823c88f94ea3bebe895a70e2d31818e094d
Sophia <sophia.shigueoka@gmail.com>
2019-05-31 14:43:04 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,2 @@
-def troca_ingrediente(self, ingrediente, combo):
-    #     self.combo += ingrediente
-    #     center = self.rect.center
-    #     if self.combo in self.images:
-    #         self.image = self.images[self.combo]
-    #     else:
-    #         self.image = self.images['ERRO']
-    #         print("Esse burrito vai pro lixo")
-    #     self.rect = self.image
-    #     self.rect.center = center
+except:
+#     pass
