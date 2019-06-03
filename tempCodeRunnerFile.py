1f68900396ada40dac4b5ae71fa70d2e9fa7bab8
Sophia <sophia.shigueoka@gmail.com>
2019-06-03 14:46:21 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-except:
-    pass
+def n_burritos_prontos(n_burritos_prontos):
+#     f = pygame.font.Font(None, 20)
+#     numero = f.render(str(n_burritos_prontos), True)
+#     screen.blit(numero, [100, 100])
