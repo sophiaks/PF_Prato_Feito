382f0864ebb870c8f378884ab5a64b1a2ec7a86d
Sophia <sophia.shigueoka@gmail.com>
2019-06-04 09:48:41 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-running = True
-# while running:
-#     telainicio = pygame.transform.scale(
-#         pygame.image.load('Telainicio.png'), (1000, 750))
-#     pygame.Surface.blit(telainicio)
-#     for event in pygame.event.get():
-#         if event.type == pygame.key.get_pressed()[pygame.K_SPACE]:
-#             running = False
+black
